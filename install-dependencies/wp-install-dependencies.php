aad2a444d41a305a1cf623ff1218e3d86e9b0481
Andy Fragen <andy@thefragens.com>
2016-01-26 22:25:06 -0800

    add in methods for 'install-github-updater'

--- install-dependencies/wp-install-dependencies.php
+++ install-dependencies/wp-install-dependencies.php
@@ -40,26 +40,32 @@ if ( ! class_exists( 'WP_Install_Dependencies' ) ) {
 	 */
 	class WP_Install_Dependencies {
 
+		protected $config;
+
 		/**
 		 * Holds plugin dependency data from wp-dependencies.json
+		 *
 		 * @var
 		 */
 		protected $dependency;
 
 		/**
 		 * Holds names of installed dependencies for admin notices.
+		 *
 		 * @var
 		 */
 		protected static $notices;
 
 		/**
 		 * Holds data of uninstalled dependencies.
+		 *
 		 * @var
 		 */
 		protected $not_installed;
 
 		/**
 		 * WP_Install_Dependencies constructor.
+		 *
 		 * @param $config
 		 */
 		public function __construct( $config ) {
@@ -78,10 +84,82 @@ if ( ! class_exists( 'WP_Install_Dependencies' ) ) {
 			/*
 			 * Exit for malformed json files.
 			 */
-			if ( empty( $config ) ) {
-				return false;
+			if ( is_null( $config ) ) {
+				return array( 'status' => 'error', 'message' => 'JSON file format error.' );
 			}
-			$this->prepare_json( $config );
+			$this->config = $this->prepare_json( $config );
+
+			add_action( 'admin_footer', array( $this, 'admin_footer' ) );
+			add_action( 'wp_ajax_github_updater', array( $this, 'ajax_router' ) );
+
+			add_action( 'admin_init', array( $this, 'admin_init' ) );
+			add_action( 'admin_notices', array( $this, 'admin_notices' ) );
+			add_action( 'network_admin_notices', array( $this, 'admin_notices' ) );
+		}
+
+		/**
+		 * Determine if dependency is active or installed
+		 */
+		function admin_init() {
+			if ( get_transient( 'github_updater_dismiss_notice' ) ) {
+				return;
+			}
+			if ( $this->is_installed() ) {
+				if ( ! is_plugin_active( $this->dependency->slug ) ) {
+					$this->message = array(
+						'action' => 'activate',
+						'text'   => sprintf( __( 'Please activate the %s plugin.' ), $this->dependency->name )
+					);
+				}
+			} else {
+				$this->message = array(
+					'action' => 'install',
+					'text'   => sprintf( __( 'The %s plugin is required.' ), $this->dependency->name )
+				);
+			}
+		}
+
+		/**
+		 * Register jQuery AJAX
+		 */
+		function admin_footer() {
+			?>
+			<script>
+				(function ($) {
+					$(function () {
+						$(document).on('click', '.ghu-button', function () {
+							var $this = $(this);
+							$('.github-updater p').html('Running...');
+							$.post(ajaxurl, {
+								action: 'github_updater',
+								method: $this.attr('data-action')
+							}, function (response) {
+								$('.github-updater p').html(response);
+							});
+						});
+						$(document).on('click', '.github-updater .notice-dismiss', function () {
+							$.post(ajaxurl, {
+								action: 'github_updater',
+								method: 'dismiss'
+							});
+						});
+					});
+				})(jQuery);
+			</script>
+			<?php
+		}
+
+		/**
+		 * AJAX router
+		 */
+		function ajax_router() {
+			$method    = isset( $_POST['method'] ) ? $_POST['method'] : '';
+			$whitelist = array( 'install', 'activate', 'dismiss' );
+			if ( in_array( $method, $whitelist ) ) {
+				$response = $this->$method();
+				echo $response['message'];
+			}
+			wp_die();
 		}
 
 		/**
@@ -99,11 +177,11 @@ if ( ! class_exists( 'WP_Install_Dependencies' ) ) {
 					continue;
 				}
 
-				$download_link = null;
+				$download_link                = null;
 				$dependency->dependent_plugin = $dependent_plugin;
-				$path = parse_url( $dependency->uri, PHP_URL_PATH );
-				$owner_repo = trim( $path, '/' );  // strip surrounding slashes
-				$owner_repo = str_replace( '.git', '', $owner_repo ); //strip incorrect URI ending
+				$path                         = parse_url( $dependency->uri, PHP_URL_PATH );
+				$owner_repo                   = trim( $path, '/' );  // strip surrounding slashes
+				$owner_repo                   = str_replace( '.git', '', $owner_repo ); //strip incorrect URI ending
 
 				switch ( $dependency->git ) {
 					case 'github':
@@ -114,7 +192,7 @@ if ( ! class_exists( 'WP_Install_Dependencies' ) ) {
 						$dependency->download_link = $download_link;
 						break;
 					case 'bitbucket':
-						$download_link = 'https://bitbucket.org/' . $owner_repo . '/get/' . $dependency->branch . '.zip';
+						$download_link             = 'https://bitbucket.org/' . $owner_repo . '/get/' . $dependency->branch . '.zip';
 						$dependency->download_link = $download_link;
 						break;
 					case 'gitlab':
@@ -127,7 +205,7 @@ if ( ! class_exists( 'WP_Install_Dependencies' ) ) {
 						break;
 				}
 
-				$this->dependency = $dependency;
+				$this->dependency            = $dependency;
 				$this->dependency->installed = $this->is_installed();
 
 				if ( ! $this->is_installed() ) {
@@ -145,6 +223,7 @@ if ( ! class_exists( 'WP_Install_Dependencies' ) ) {
 		 */
 		function is_installed() {
 			$plugins = get_plugins();
+
 			return isset( $plugins[ $this->dependency->slug ] );
 		}
 
@@ -155,28 +234,39 @@ if ( ! class_exists( 'WP_Install_Dependencies' ) ) {
 			if ( ! $this->is_installed() ) {
 				add_filter( 'upgrader_source_selection', array( &$this, 'upgrader_source_selection' ), 10, 2 );
 
-				$skin = new WPID_Plugin_Installer_Skin( array(
-					'type'      => 'plugin',
-					'nonce'     => wp_nonce_url( $this->dependency->download_link ),
-					) );
+				$skin     = new WPID_Plugin_Installer_Skin( array(
+					'type'  => 'plugin',
+					'nonce' => wp_nonce_url( $this->dependency->download_link ),
+				) );
 				$upgrader = new Plugin_Upgrader( $skin );
-				$upgrader->install( $this->dependency->download_link );
+				$result   = $upgrader->install( $this->dependency->download_link );
+
+				if ( is_wp_error( $result ) ) {
+					return array( 'status' => 'error', 'message' => $result->get_error_message() );
+				}
 				wp_cache_flush();
+				$result = $this->activate();
+				if ( 'error' == $result['status'] ) {
+					return $result;
+				}
+
 
 				if ( ! $this->dependency->optional ) {
-					activate_plugin( $this->dependency->slug, null, true );
+					//activate_plugin( $this->dependency->slug, null, true );
 				}
 
 				if ( is_admin() && ! defined( 'DOING_AJAX' ) &&
 				     $upgrader->skin->result
 				) {
-					self::$notices[] = $this->dependency->name;
-					add_action( 'admin_notices', array( &$this, 'message' ) );
-					add_action( 'network_admin_notices', array( &$this, 'message' ) );
+					//self::$notices[] = $this->dependency->name;
+					//add_action( 'admin_notices', array( &$this, 'message' ) );
+					//add_action( 'network_admin_notices', array( &$this, 'message' ) );
 				}
 
 				unset( $this->not_installed[ dirname( $this->dependency->slug ) ] );
 				$this->dependency->installed = true;
+
+				return array( 'status' => 'ok', 'message' => sprintf( __( '%s has been installed and activated.' ), $this->dependency->name ) );
 			}
 		}
 
@@ -207,10 +297,10 @@ if ( ! class_exists( 'WP_Install_Dependencies' ) ) {
 				echo '<tr class="plugin-update-tr" data-slug="' . dirname( $this->dependency->dependent_plugin ) . '" data-plugin="' . $this->dependency->dependent_plugin . '"><td colspan="' . $wp_list_table->get_column_count() . '" class="plugin-update colspanchange"><div class="update-message update-ok">';
 				print( $not_installed['name'] . ' ' . esc_html__( 'is listed as an optional dependency.' ) . ' ' );
 				print( '<a href="' . $not_installed['link'] . '">' . esc_html__( 'Download Now' ) . '</a><br>' );
+
 				echo '</div></td></tr>';
 			}
-
-			print('<script>jQuery(".active[data-plugin=\'' . $this->dependency->dependent_plugin . '\']").addClass("update");</script>');
+			print( '<script>jQuery(".active[data-plugin=\'' . $this->dependency->dependent_plugin . '\']").addClass("update");</script>' );
 		}
 
 		/**
@@ -237,13 +327,50 @@ if ( ! class_exists( 'WP_Install_Dependencies' ) ) {
 				?>
 				<div class="updated notice is-dismissible">
 					<p>
-						<?php echo $notice;  _e( ' has been installed and activated as a dependency.' ) ?>
+						<?php echo $notice;
+						_e( ' has been installed and activated as a dependency.' ) ?>
 					</p>
 				</div>
 				<?php
 			}
 		}
 
+		/**
+		 * Activate GHU
+		 */
+		public function activate() {
+			$result = activate_plugin( $this->dependency->slug );
+			if ( is_wp_error( $result ) ) {
+				return array( 'status' => 'error', 'message' => $result->get_error_message() );
+			}
+
+			return array( 'status' => 'ok', 'message' => sprintf( __( '%s has been activated.' ), $this->dependency->name ) );
+		}
+
+		/**
+		 * Dismiss admin notice for a week
+		 */
+		public function dismiss() {
+			//set_transient( 'github_updater_dismiss_notice', 'yes', ( 60 * 60 * 24 * 7 ) );
+
+			return array( 'status' => 'ok', 'message' => '' );
+		}
+
+		/**
+		 * Display admin notices / action links
+		 */
+		public function admin_notices() {
+			if ( ! empty( $this->message ) ) {
+				$action = $this->message['action'];
+				$notice = $this->message['text'];
+				$notice .= ' <a href="javascript:;" class="ghu-button" data-action="' . $action . '">' . ucfirst( $action ) . ' Now &raquo;</a>';
+				?>
+				<div class="updated notice is-dismissible github-updater">
+					<p><?php echo $notice; ?></p>
+				</div>
+				<?php
+			}
+		}
 	}
 
 	require_once ABSPATH . 'wp-admin/includes/class-wp-upgrader.php';
