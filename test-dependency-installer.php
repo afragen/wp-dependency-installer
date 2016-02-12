a7566cc8be52f1f59eda1caa44cbafcaa0f7ff9c
Andy Fragen <andy@thefragens.com>
2016-02-12 11:42:44 -0800

    fix branch

--- test-dependency-installer.php
+++ test-dependency-installer.php
@@ -9,7 +9,7 @@
  * License: GNU General Public License v2
  * License URI: http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  * GitHub Plugin URI: https://github.com/afragen/wp-dependency-installer
- * GitHub Branch: develop
+ * GitHub Branch: master
  * Requires WP: 4.0
  * Requires PHP: 5.3
  */
